f3354d5ec089bd85e9a738e0ab456a4459bcb66a
rominavita <romiv.vita@gmail.com>
2024-07-03 21:29:27 +0200

    Test(Cart)

--- config/browserstack/wdio.android.app.conf.ts
+++ config/browserstack/wdio.android.app.conf.ts
@@ -2,18 +2,20 @@ import { config as sharedConfig } from './wdio.shared.bs.conf.ts';
 
 export const config = {
   ...sharedConfig,
-
-  capabilities: [
-    {
-      ...sharedConfig.capabilities[0],
-      'appium:app': process.env.ANDROID_APP,
-      'bstack:options': {
-        deviceName: 'Samsung Galaxy S10e',
-        platformName: 'android',
-        platformVersion: '9.0',
-        projectName: 'SAT Android App',
-        idleTimeout: 180,
+  ...{
+    capabilities: [
+      {
+        ...sharedConfig.capabilities[0],
+        'appium:app': process.env.ANDROID_APP,
+        'bstack:options': {
+          deviceName: 'Samsung Galaxy S10e',
+          platformName: 'android',
+          platformVersion: '9.0',
+          projectName: process.env.BUILD_NAME + ' Android App',
+          buildName: 'Suite: ' + process.env.BUILD_TAGS,
+          idleTimeout: 180,
+        },
       },
-    },
-  ],
+    ],
+  },
 };
--- config/browserstack/wdio.ios.app.conf.ts
+++ config/browserstack/wdio.ios.app.conf.ts
@@ -2,18 +2,19 @@ import { config as sharedConfig } from './wdio.shared.bs.conf.ts';
 
 export const config = {
   ...sharedConfig,
-
-  capabilities: [
-    {
-      ...sharedConfig.capabilities[0],
-      'appium:app': process.env.IOS_APP,
-      'bstack:options': {
-        deviceName: 'iPhone 11 Pro Max',
-        platformName: 'iOS',
-        platformVersion: '13.5',
-        projectName: 'SAT iOS App',
-        idleTimeout: 180,
+  ...{
+    capabilities: [
+      {
+        ...sharedConfig.capabilities[0],
+        'appium:app': process.env.IOS_APP,
+        'bstack:options': {
+          deviceName: 'iPhone 11 Pro Max',
+          platformName: 'iOS',
+          projectName: process.env.BUILD_NAME + ' iOS App',
+          buildName: 'Suite: ' + process.env.BUILD_TAGS,
+          idleTimeout: 180,
+        },
       },
-    },
-  ],
+    ],
+  },
 };
